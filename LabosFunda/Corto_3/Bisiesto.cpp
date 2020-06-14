ca310a5867b1c00e35ba8711afb90df81a6b56f0
Peraza <00143320@gmail.com>
2020-06-13 20:43:38 -0600

    Examen 3 de Programacion

---
+++ LabosFunda/Corto_3/Bisiesto.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+bool Bisiesto(int);
+
+int main()
+{
+    //Declaracion de variables
+    int year = 0;
+    //Peticion de datos
+    cout << "Ingrese el año a determinar si es bisiesto o no ";
+    cin >> year;
+    
+    if(Bisiesto(year) == true)
+        cout << "El año " << year << " es bisiesto";
+    else
+        cout << "El año " << year << " no es bisiesto";
+
+    return 0;
+}
+
+bool Bisiesto(int year)
+{
+    //Comprobamos si el anio cumple alguna de las condiciones
+    if( ( year % 400 ) == 0)
+        return true;
+    else if( ( year % 4 == 0 ) && (year % 100 != 0) )
+        return true;
+    else 
+        return false; //si no se cumplen, el anio no es bisiesto  
+}
